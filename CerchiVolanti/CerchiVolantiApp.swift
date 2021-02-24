bcfa76e22b12332fcfdc8124cec79a3b5a010b5c
Elechim01 <mikymanny015@gmail.com>
2021-02-24 16:21:35 +0100

    Initial Commit

---
+++ CerchiVolanti/CerchiVolantiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CerchiVolantiApp.swift
+//  CerchiVolanti
+//
+//  Created by Michele Manniello on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CerchiVolantiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
